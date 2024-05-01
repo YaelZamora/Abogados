9439db529e324d5f14223f6797cb1efead7aedbe
YaelZamora <ajedrezkasparov@gmail.com>
2024-05-01 12:58:29 -0600

    Initial Commit

---
+++ abogados/abogadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  abogadosApp.swift
+//  abogados
+//
+//  Created by Yael Javier Zamora Moreno on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct abogadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
